5f032529663c9065e79e519bb98189adf3c83494
edmancota <edmancota2017@gmail.com>
2022-05-11 17:04:15 -0600

    Fixed dep error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable consistent-return */
+/* eslint-disable object-curly-newline */
 /* eslint-disable no-unused-expressions */
 import { useState, useEffect } from "react";
 import { Button, Flex, useColorModeValue, Text } from "@chakra-ui/react";
